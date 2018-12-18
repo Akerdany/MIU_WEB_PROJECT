b61316343fabeed48406aade87d1f967a23e6454
foadelamoury <foad.osama@hotmail.com>
2018-12-18 15:46:11 +0200

    fixed the problem of the two nested while loops

--- php/includes/comment0.inc.php
+++ php/includes/comment0.inc.php
@@ -5,7 +5,7 @@ function setComments($conn)
 
   if (isset($_POST['commentSubmit'])) {
 
-      $id=$_SESSION['id'];
+     $id=$_SESSION['id'];
       $date=$_POST['date'];
       $message=$_POST['message'];
 
@@ -19,16 +19,43 @@ function setComments($conn)
 
  function getComments($conn)
 {
-  $sql= "SELECT user.email,user.id,comments.*
-  FROM user
-  INNER JOIN comments ON user.id=comments.userId";
+  $sql= 'SELECT * FROM comments';
   $result= mysqli_query($conn,$sql);
+//  $email=$_SESSION['email'];
   while ($row=mysqli_fetch_assoc($result)) {
   echo "<div class='comment-box'><p>";
+  $sqlFinder= "SELECT user.email,user.id
+  FROM user
+  INNER JOIN comments ON user.id=comments.userId";
+
+  $resultFinder= mysqli_query($conn,$sqlFinder);
+$email='';
+ while($rowFinder =mysqli_fetch_array($resultFinder)){
 
 
-echo $row['email']."<br>";
+
+
+
+
+
+
+
+
+
+
+   if ($row['userId']==$rowFinder['id']) {
+
+
+
+echo   $rowFinder['email']."<br>";
+break;
+
+}
+  }
+//echo $row['userId']."<br>";
 echo $row['date']."<br>";
+
+
 echo nl2br($row['message']); // nl2br is a new line 2 breaks <br>
 echo "</p></div>";
 
