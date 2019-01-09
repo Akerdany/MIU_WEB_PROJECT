9a49b34979759d7121e621bb4e1f1fb9f5a020e4
foadelamoury <foad.osama@hotmail.com>
2019-01-09 22:02:47 +0200

    Create accounting.php

---
+++ php/accounting.php
@@ -0,0 +1,37 @@
+<?php 
+require_once("Database_Connection.php");
+   $sqlInsurance="select a.*  from
+   accounting a ";
+
+   $resultInsurance = mysqli_query($conn,$sqlInsurance);
+ if(mysqli_num_rows($resultInsurance) > 0){
+                echo"<table border='1'>
+                    <tr>
+                    <td>ID</td>
+                    <td>Disease</td>
+                    <td>Medicine Type </td>
+                    <td>medicineType</td>
+                    <td>medicalCardNumber</td>
+					<td>maximumInsuranceCost</td>
+					<td>email</td>
+                    </tr>";
+                while($row = mysqli_fetch_array($resultInsurance)){
+                    echo "<tr>";
+                    echo "<td>" .$row['id']. "</td>";
+                    echo "<td>" .$row['disease']. "</td>";
+                    echo "<td>".$row['medicineType']."</td>";
+                    echo "<td>" .$row['medicalCardNumber']. "</td>";
+                    echo "<td>".$row['medicinePrice']."</td>";
+					echo "<td>".$row['maximumInsuranceCost']."</td>";
+					echo "<td>".$row['email']."</td>";
+
+					
+                    echo "</tr>";
+
+
+                }
+                    echo "</table>";
+            }
+
+
+?>
