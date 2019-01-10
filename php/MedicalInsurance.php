bc676b1ff9272a96cd6e34192f0651925e60e141
foadelamoury <foad.osama@hotmail.com>
2019-01-11 00:46:43 +0200

    will continue in the head of doctors and database changes
    What I changed in database
    Department: newDepartment (Management)
    Employee and User:inserted rows of hr and head of doctors

--- php/MedicalInsurance.php
+++ php/MedicalInsurance.php
@@ -4,14 +4,27 @@ require_once("Database_Connection.php");
    $sqlInsurance="select m.*,u.firstName  from
    medicalinsurance m JOIN employee e ON e.medicalInsuranceId = m.id
    JOIN user u ON u.id=e.userId";
-	//will be in a function, it is when any employee except HR enters the table of the medical insurance
+	//will be in a function, it is when any employee except HR and Manager and Head of doctors enters the table of the medical insurance
 	
 	$sqlInsuranceEmployee="select u.firstName  from
     employee e 
    JOIN user u ON u.id=e.userId
    AND e.departmentId NOT IN (2,10)
-   
+	
    AND e.userId='".$_SESSION['userId']."'";
+   //The head of doctors
+	$sqlInsuranceDoctor="select e.positionId,e.departmentId  from
+    employee e 
+   JOIN user u ON u.id=e.userId
+   AND e.departmentId =5
+	AND e.positionId=1
+   AND e.userId='".$_SESSION['userId']."'";
+    $resultInsuranceDoctor = mysqli_query($conn,$sqlInsuranceDoctor);
+	 if(mysqli_num_rows($resultInsuranceEmployee) > 0)
+	{
+			
+	}
+   //the head of doctors section end
     $resultInsuranceEmployee = mysqli_query($conn,$sqlInsuranceEmployee);
     if(mysqli_num_rows($resultInsuranceEmployee) > 0)
 	{	
