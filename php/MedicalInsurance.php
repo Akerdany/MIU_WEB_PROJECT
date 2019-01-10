a4edc09575c8a68a246e4d487b66ed4eeb6bbd41
foadelamoury <foad.osama@hotmail.com>
2019-01-10 23:09:49 +0200

    Update MedicalInsurance.php

--- php/MedicalInsurance.php
+++ php/MedicalInsurance.php
@@ -1,30 +1,39 @@
 <?php 
 require_once("Database_Connection.php");
-   $sqlInsurance="select m.* ,u.email from
+   $sqlInsurance="select m.*,u.firstName  from
    medicalinsurance m JOIN employee e ON e.medicalInsuranceId = m.id
-   JOIN user u ON u.id=e.userId   ";
-
+   JOIN user u ON u.id=e.userId";
+	//will be in a function, it is when the HR employee enters the table of the medical insurance
+	
+	$sqlInsuranceEmployee="select u.firstName  from
+    employee e 
+   JOIN user u ON u.id=e.userId";
+	
+	//ends here
    $resultInsurance = mysqli_query($conn,$sqlInsurance);
  if(mysqli_num_rows($resultInsurance) > 0){
+	 
                 echo"<table border='1'>
                     <tr>
                     <td>ID</td>
+					<td>First Name</td>
                     <td>Disease</td>
                     <td>Medicine Type </td>
                     <td>medicineType</td>
                     <td>medicalCardNumber</td>
 					<td>maximumInsuranceCost</td>
-					<td>email</td>
+					
                     </tr>";
                 while($row = mysqli_fetch_array($resultInsurance)){
                     echo "<tr>";
                     echo "<td>" .$row['id']. "</td>";
+					echo "<td>".$row['firstName']."</td>";
                     echo "<td>" .$row['disease']. "</td>";
                     echo "<td>".$row['medicineType']."</td>";
                     echo "<td>" .$row['medicalCardNumber']. "</td>";
                     echo "<td>".$row['medicinePrice']."</td>";
 					echo "<td>".$row['maximumInsuranceCost']."</td>";
-					echo "<td>".$row['email']."</td>";
+					
 
 					
                     echo "</tr>";
