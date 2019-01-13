8a1354af4c3c199e0729ab42fc109bb806b34e6e
Ahmed ElKerdany <akerdany@gmail.com>
2019-01-13 20:14:13 +0200

    editAccount

--- php/displayUsers.php
+++ php/displayUsers.php
@@ -1,77 +1,104 @@
-<?php
-    session_start();
-    require_once("Database_Connection.php");
-    require("sidebar.php");
-
-    $sql = "select * from user";
-    $result = mysqli_query($conn,$sql);
-
-    if(mysqli_num_rows($result) > 0){
-        echo"<table border='1' class='Table_Of_Users'>
-            <tr>
-            <th>ID</th>
-            <th>Email</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Family Name</th>
-            <th>Gender</th>
-            <th>Nationality</th>
-            <th>Date of Birth</th>
-            <th>Work Number</th>
-            <th>Phone Number</th>
-            <th>Home Number</th>
-            <th>Date & Time Joined</th>
-            <th>Account Status</th>
-            <th>SSN</th>
-            <th>Type of User</th>
-            </tr>";
-
-        while($row = mysqli_fetch_array($result)){
-            echo "<tr>";
-            echo "<td>" .$row['id']. "</td>";
-            echo "<td>" .$row['email']. "</td>";
-            echo "<td>" .$row['firstName']. "</td>";
-            echo "<td>".$row['lastName']."</td>";
-            echo "<td>" .$row['familyName']. "</td>";
-            echo "<td>" .$row['gender']. "</td>";
-            echo "<td>".$row['nationality']."</td>";
-            echo "<td>" .$row['dateOfBirth']. "</td>";
-            echo "<td>".$row['workNumber']."</td>";
-            echo "<td>" .$row['phoneNumber']. "</td>";
-            echo "<td>" .$row['homeTelephoneNumber']. "</td>";
-            echo "<td>".$row['dateJoined']."</td>";
-            $status = mysqli_query($conn,"SELECT * FROM status WHERE id='".$row["statusId"]."'");
-            if($r = mysqli_fetch_array($status)){
-                echo "<td>" .$r['statusName']. "</td>";
-            }
-            echo "<td>".$row['ssn']."</td>";
-            $type = mysqli_query($conn,"SELECT * FROM type WHERE id='".$row["typeId"]."'");
-            if($r1 = mysqli_fetch_array($type)){
-                echo "<td>" .$r1['typeName']. "</td>";
-            }
-            echo "</tr>";
-        }
-            echo "</table>";
-    }
-    include 'Comments.php';
-    // mysqli_close($conn);    
-?>
-
-<style>
-        body
-        {
-            background-color:teal;
-        }
+<!DOCTYPE <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Display Users</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+        <script src="main.js"></script>
         
-        .Table_Of_Users
-        {
-            background-color:linen;
-            border:5px;
-            top:20px;
-            left:20px;
-            width:1520px;
-            height:300px;
-            text-align:center;
-            font-size:15px;
-        } 
-</style>
+        <style>
+            body
+            {
+                background-color:teal;
+            }
+            
+            .Table_Of_Users
+            {
+                background-color:linen;
+                border:5px;
+                top:20px;
+                left:20px;
+                width:1520px;
+                height:300px;
+                text-align:center;
+                font-size:15px;
+            } 
+        </style>
+
+        <script>
+            var table = document.getElementById("table"),rIndex;
+
+            for(var i = o; i < table.rows.length; i++){
+                table.rows[i].onclick = function(){
+                    rIndex = this.rowIndex;
+                    alert(rIndex);
+                    // document.getElementById()
+                };
+            }
+        </script>
+    </head>
+
+    <body>
+        <?php
+            session_start();
+            require_once("Database_Connection.php");
+            require("sidebar.php");
+
+            $sql = "select * from user";
+            $result = mysqli_query($conn,$sql);
+
+            if(mysqli_num_rows($result) > 0){
+                echo"<table id='table' border='1' class='Table_Of_Users'>
+                    <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Family Name</th>
+                    <th>Gender</th>
+                    <th>Nationality</th>
+                    <th>Date of Birth</th>
+                    <th>Work Number</th>
+                    <th>Phone Number</th>
+                    <th>Home Number</th>
+                    <th>Date & Time Joined</th>
+                    <th>Account Status</th>
+                    <th>SSN</th>
+                    <th>Type of User</th>
+                    </tr>";
+
+                while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo "<td>" .$row['id']. "</td>";
+                    echo "<td>" .$row['email']. "</td>";
+                    echo "<td>" .$row['firstName']. "</td>";
+                    echo "<td>".$row['lastName']."</td>";
+                    echo "<td>" .$row['familyName']. "</td>";
+                    echo "<td>" .$row['gender']. "</td>";
+                    echo "<td>".$row['nationality']."</td>";
+                    echo "<td>" .$row['dateOfBirth']. "</td>";
+                    echo "<td>".$row['workNumber']."</td>";
+                    echo "<td>" .$row['phoneNumber']. "</td>";
+                    echo "<td>" .$row['homeTelephoneNumber']. "</td>";
+                    echo "<td>".$row['dateJoined']."</td>";
+                    $status = mysqli_query($conn,"SELECT * FROM status WHERE id='".$row["statusId"]."'");
+                    if($r = mysqli_fetch_array($status)){
+                        echo "<td>" .$r['statusName']. "</td>";
+                    }
+                    echo "<td>".$row['ssn']."</td>";
+                    $type = mysqli_query($conn,"SELECT * FROM type WHERE id='".$row["typeId"]."'");
+                    if($r1 = mysqli_fetch_array($type)){
+                        echo "<td>" .$r1['typeName']. "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            include 'Comments.php';
+            // mysqli_close($conn);    
+        ?>
+
+    </body>
+</html>
--- php/editAccount.php
+++ php/editAccount.php
@@ -7,12 +7,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#editPassword").click(function(){
+                $("#Form").hide();
+                $("#passwordForm").fadeIn(1000);
+            });
+        });
+
+        $(document).ready(function(){
+            $("#getBack").click(function(){
+                $("#passwordForm").hide();
+                $("#Form").fadeIn(1000);
+            });
+        });
+
+    </script>
+
 </head>
 
 <body>
     <?php
         session_start();
         require_once("Database_Connection.php");
+        require("sidebar.php");
 
         if(isset($_POST['submit'])){
             $first = $_POST['firstName'];
@@ -30,21 +49,63 @@
         mysqli_close($conn);
     ?>
 
-    <form name="editForm" action="" method="post">
+    <div id="passwordForm" style="display:none;">
+        <form name="passwordEdit" action="" method="post">
+            <b> Old Password: </b>
+            <input type="password" name="oldPassword" placeholder="Old Password"><br>
 
-       <b> First Name: </b>
-        <input type="text" name="firstName" value="<?php echo $_SESSION["firstName"];?>" placeholder="First Name.."><br>
+            <b> New Password: </b>
+            <input type="password" name="newPassword" placeholder="New Password"><br>
+            
+            <b> Confirm New Password: </b>
+            <input type="password" name="confirmNewPassword" placeholder="Confirm New Password"><br>
 
-       <b> Last Name:</b>
-        <input type="text" name="lastName" value="<?php echo $_SESSION["lastName"];?>" placeholder="Last Name.."><br>
+            <input type="submit" name="newPass">
+        </form>
+        <button id='getBack' name='getBack' type='button'>Get Back</button><br>
+    </div>
 
-        <b>Password:</b>
-        <input type="text" name="password" value="<?php echo $_SESSION["password"];?>" placeholder="Password"><br>
+    <div id="Form">
+        <form name="editForm" action="" method="post">
 
-        <input type="submit" name="submit">
-        <input type="submit" name="deleteAccount" value="Delete your Account">
-    </form>
+            <b> First Name: </b>
+            <input type="text" name="firstName" value="<?php echo $_SESSION["firstName"];?>" placeholder="First Name.."><br>
 
+            <b> Last Name: </b>
+            <input type="text" name="lastName" value="<?php echo $_SESSION["lastName"];?>" placeholder="Last Name.."><br>
+
+            <b> Family Name: </b>
+            <input type="text" name="lastName" value="<?php echo $_SESSION["familyName"];?>" placeholder="Family Name.."><br>
+            
+            <b> Gender: </b>
+            <input type="text" name="lastName" value="<?php echo $_SESSION["gender"];?>" placeholder="Gender"><br>
+            
+            <b> Nationality: </b>
+            <input type="text" name="lastName" value="<?php echo $_SESSION["nationality"];?>" placeholder="Nationality"><br>
+
+            <b> Date of Birth: </b>
+            <input type="date" name="lastName" value="<?php echo $_SESSION["dateOfBirth"];?>"><br>
+
+            <b> Work Number: </b>
+            <input type="number" name="lastName" value="<?php echo $_SESSION["workNumber"];?>" placeholder="Work Number"><br>
+            
+            <b> Phone Number: </b>
+            <input type="number" name="lastName" value="<?php echo $_SESSION["phoneNumber"];?>" placeholder="Phone Number"><br>
+
+            <b> Home Number: </b>
+            <input type="number" name="lastName" value="<?php echo $_SESSION["homeNumber"];?>" placeholder="Home Number"><br>
+
+            <b> SSN: </b>
+            <input type="number" name="lastName" value="<?php echo $_SESSION["ssn"];?>" placeholder="SSN"><br>
+
+            <b> Email: </b>
+            <input type="text" name="lastName" value="<?php echo $_SESSION["username"];?>" placeholder="Email"><br>
+
+            <button id='editPassword' name='editPassword' type='button'>Edit your Password</button>
+            <input type="submit" name="submit" value="Edit">
+            <input type="submit" name="deleteAccount" value="Delete your Account">
+        </form>
+    </div>
 </body>
 
 </html>
