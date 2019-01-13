f994ff81745d4b9ab8afb3a993dfc7f8e4de024f
Mark Nabil Mehanni <marknabil_ms@live.com>
2019-01-13 15:48:46 +0200

    css

--- php/index.php
+++ php/index.php
@@ -2,50 +2,7 @@
 
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-            body {
-                font-family: "Lato", sans-serif;
-            }
-
-            .sidenav {
-                height: 100%;
-                width: 0;
-                position: fixed;
-                z-index: 1;
-                top: 0;
-                left: 0;
-                background-color: #111;
-                overflow-x: hidden;
-                transition: 0.5s;
-                padding-top: 60px;
-            }
-
-            .sidenav a {
-                padding: 8px 8px 8px 32px;
-                text-decoration: none;
-                font-size: 25px;
-                color: #818181;
-                display: block;
-                transition: 0.3s;
-            }
-
-            .sidenav a:hover {
-                color: #f1f1f1;
-            }
-
-            .sidenav .closebtn {
-                position: absolute;
-                top: 0;
-                right: 25px;
-                font-size: 36px;
-                margin-left: 50px;
-            }
-
-            @media screen and (max-height: 450px) {
-            .sidenav {padding-top: 15px;}
-            .sidenav a {font-size: 18px;}
-            }
-        </style>
+       
     </head>
 
     <body>
@@ -76,15 +33,15 @@
                     }
                 }
 
-                echo"<h2>Account Information: <h2><br>";
-                echo"Email: ".$_SESSION["username"]."<br>";
-                echo"Password: ".$_SESSION["password"]."<br>";
-                echo"Gender: ".$_SESSION["gender"]."<br>";
-                echo"Nationality: ".$_SESSION["nationality"]."<br>";
-                echo"Date of Birth: ".$_SESSION["dateOfBirth"]."<br>";
-                echo"Work Number: ".$_SESSION["workNumber"]."<br>";
-                echo"Phone Number: ".$_SESSION["phoneNumber"]."<br>";
-                echo"Home Number: ".$_SESSION["homeNumber"]."<br>";
+                echo"<h2>Account Information: <h2> <br>";
+                echo"Email: ".$_SESSION["username"]."<br> <br>";
+                echo"Password: ".$_SESSION["password"]."<br> <br>";
+                echo"Gender: ".$_SESSION["gender"]."<br> <br>";
+                echo"Nationality: ".$_SESSION["nationality"]."<br> <br>";
+                echo"Date of Birth: ".$_SESSION["dateOfBirth"]."<br> <br>";
+                echo"Work Number: ".$_SESSION["workNumber"]."<br> <br>";
+                echo"Phone Number: ".$_SESSION["phoneNumber"]."<br> <br>";
+                echo"Home Number: ".$_SESSION["homeNumber"]."<br> <br>";
                 echo"SSN: ".$_SESSION["ssn"]."<br>";
             ?>
         </div>
@@ -119,4 +76,74 @@
     </script>
 
     </body>
+
+    <style>
+            body {
+                font-family: "Lato", sans-serif;
+                Background-color: linen;
+                background-image: url("../pictures/Office.jpg");
+                background-repeat: no-repeat;
+                background-size:100%;
+                position:relative;
+
+            }
+            .welcome 
+            { 
+                float:right;
+                position:relative; 
+                margin-right:470px;
+                padding:10px;
+                border-radius: 5px;
+                border-left: 6px solid navy;
+                border-right: 6px solid navy;
+                background-color:#fad78b;
+                color:black;
+            }
+
+            .sidenav
+             {
+                height: 100%;
+                width: 300px;
+                position: fixed;
+                z-index: 1;
+                top: 0px;
+                left: 0px;
+                background-color: #111;/*deh loun elside bar lm ayfta7 */
+                overflow-x: hidden;
+                transition: 0.5s;
+                padding-top:0px;
+                color:white;
+            }
+
+            .sidenav a {
+                padding: 20px /*spaces been elklaam gwa side bar*/
+                 0px
+                 8px 
+                20px;
+                text-decoration: none;
+                font-size: 25px;
+                color: #818181; /* dah lun font*/
+                display: block;
+                transition: 0.3s;
+            }
+
+            .sidenav a:hover {
+                color: white;
+            }
+
+            .sidenav .closebtn {
+                position: absolute;
+                top: 0;
+                right: 25px;
+                font-size: 36px;
+                margin-left: 50px;
+            }
+
+            @media screen and (max-height: 450px) {
+            .sidenav {padding-top: 15px;}
+            .sidenav a {font-size: 18px;}
+            }
+        </style>
+
+        
 </html>
