1b509ede77b16efe2c9874ab4cc8b27b70413eec
Mark Nabil Mehanni <marknabil_ms@live.com>
2019-01-15 02:29:44 +0200

    css

--- php/registration.php
+++ php/registration.php
@@ -53,18 +53,6 @@
                 });
         }
 
-        // function getResult()
-        // {
-        //     jQuery.ajax(
-        //         {
-        //             url: "backend-search.php",
-        //             data: 'term='+$("#term").val(),
-        //             type: "POST",
-        //             success:function(data2){
-        //                 $("#result").html(data2);
-        //             }
-        //         });
-        }
 
     </script>
 
